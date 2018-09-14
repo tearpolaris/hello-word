653c250f9d940cf2cef5a62474ccf4f20a35c4d9
tearpolaris <34851020+tearpolaris@users.noreply.github.com>
2018-09-14 20:08:45 +0700

    Update small_triangle.c

--- small_triangle.c
+++ small_triangle.c
@@ -14,32 +14,32 @@ void sort_by_area(triangle* tr, int n) {
 	/**
 	* Sort an array a of the length n
 	*/
-    double S0 = 0, S1 = 0;
+    double Smax = 0, S1 = 0;
     double p0, p1;
-    int tmp_a, tmp_b, tmp_c;
+    int tmp_a, tmp_b, tmp_c, max_position;
     for (int i = 0; i < n; i++) {
         max_position = i;
         p0 = tr[max_position].a + tr[max_position].b + tr[max_position].c;
-        Smax = sqrt(p0*(p0-a)*(p0-b)*(p0-c));
+        Smax = sqrt(p0*(p0-tr[max_position].a)*(p0-tr[max_position].b)*(p0-tr[max_position].c));
         for (int j = i+1; j < n-i; j++) {
           p1 = tr[j].a + tr[j].b + tr[j].c; 
-          S1 = sqrt(p1*(p1-a)*(p1-b)*(p1-c));
+          S1 = sqrt(p1*(p1-tr[j].a)*(p1-tr[j].b)*(p1-tr[j].c));
         
           if (S1 < Smax ) {          
-            tr[j].a = tmp_a;
-            tr[j].b = tmp_b;
-            tr[j].c = tmp_c;
+            tmp_a = tr[j].a;
+            tmp_b = tr[j].b;
+            tmp_c = tr[j].c;
             tr[j].a = tr[max_position].a;
             tr[j].b = tr[max_position].b;
             tr[j].c = tr[max_position].c;
             tr[max_position].a = tmp_a;
             tr[max_position].b = tmp_b;
-            ttr[max_position].c = tmp_c;
+            tr[max_position].c = tmp_c;
             max_position = j;
          }
-    }    
+    }
 }
-
+}
 int main()
 {
 	int n;
