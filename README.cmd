e7a98c675b9dfd7d6cffa84773c47eda576418d4
tearpolaris <34851020+tearpolaris@users.noreply.github.com>
2020-01-01 23:43:42 +0700

    Update README.cmd

--- README.cmd
+++ README.cmd
@@ -1,2 +1,9 @@
-
-https://www.adayroi.com/bo-ban-lam-viec-ibie-rec-z-va-ghe-eames-chan-go-ib-tdc004-120-x-60-x-75-cm-chan-trang-p-PRI833558?offer=PRI833558_NY7&search=ghe+lam+viec
+Good afternoon everyone, I'm Anh from IP Design 2. Today is my turn to conduct the afternoon speech. I hope you had a nice holiday with your
+friends, family and will have a better year than last one. Today I would like to say a little bit about a book, maybe some or many of you already
+know this book, its name is "Con Duong di Den Su Thanh Cong bang su tu te", the author is Inamori Kazuo, he is the founder of Kyocera and he 
+was the chairman of Japan airlines. This book told about the Inamori's vision of life, attitude towards the life, how to treat to the people .
+I've learned many things from this book, such as the inamori scarifies. Even in the toughest situation and related to benefit of himself, 
+he still think about his staff. As my feeling, his thinking come from the kindness, pure soul, no unfairness, no tu loi. There are more than 
+what I said, but I can't express all of them and because every people has their own perspective. If you interest, you can read this book and 
+have your own feeling. One statement I like is "Thành công đến từ sự lợi dụng thời thế nhất thời mà không quan tấm đến lợi ích của mọi người 
+sẽ không kéo dài được lâu". Once again, wish you have a good new year. That's all for my afternoon speech, thank you for your attention.
